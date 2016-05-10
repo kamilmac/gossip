540697b4eb2575646fb63b1d13584b4cb21c56a9
Kamil Macinski <kamil@blogg.se>
2016-05-10 08:23:46 +0200

    web ui routes registered

--- main.go
+++ main.go
@@ -17,13 +17,53 @@ type response struct {
     Message     string              `json:"message"`
 }
 
-type attachReq struct {
+type request struct {
     Password    string              `json:"password"`
+}
+
+type attachReq struct {
+    request
     Data        map[string]string   `json:"data"`
 }
 
 type attachRes struct {
     response    
+}
+
+type getReq struct {
+    request
+}
+
+type getRes struct {
+    Data        map[string]string   `json:"data"`
+    response
+}
+
+type setReq struct {
+    Key         string              `json:"key"`
+    Value       string              `json:"value"`
+    request
+}
+
+type setRes struct {
+    response
+}
+
+type delReq struct {
+    Key         string              `json:"key"`
+    request
+}
+
+type delRes struct {
+    response
+}
+
+type broadcastReq struct {
+    request
+}
+
+type broadcastRes struct {
+    response
 }
 
 type broadcastMsg struct {
@@ -34,6 +74,7 @@ type broadcastMsg struct {
 const (
     connectionTimeout = 10
 )
+
 var (
     pass string
     port int
@@ -66,8 +107,8 @@ func handleAttach(w http.ResponseWriter, r *http.Request) {
     for k, v := range req.Data {
         kv[string(k)] = string(v)
     }
-    deadSubscribers := handshake(kv)
-    log.Println("Handshake done. Dead subscribers: ", deadSubscribers)
+    deadSubscribers := validateSubscribers(kv)
+    log.Println("validateSubscribers done. Dead subscribers: ", deadSubscribers)
     kv = removeDeadSubscribers(kv, deadSubscribers)
     log.Println("Dead subscribers removed")
     broadcast(kv)
@@ -76,7 +117,7 @@ func handleAttach(w http.ResponseWriter, r *http.Request) {
     res.Message = "Broadcast done"
 }
 
-func handshake(store map[string]string) map[string]string {
+func validateSubscribers(store map[string]string) map[string]string {
     time.Sleep(1*time.Second)
     subscribers := extractSubscribers(store)
     deadSubscribers := make(map[string]string)
@@ -86,7 +127,7 @@ func handshake(store map[string]string) map[string]string {
         go func(name, callback string) {
             req, err := http.NewRequest("POST", callback, bytes.NewBuffer([]byte("{}")))
             if err != nil {
-                log.Println("Handshake request err: ", err)
+                log.Println("validateSubscribers request err: ", err)
                 log.Println("Removing subscriber: ", callback)
                 deadSubscribers[name] = callback
                 wg.Done()
@@ -98,7 +139,7 @@ func handshake(store map[string]string) map[string]string {
             }
             resp, err := client.Do(req)
             if err != nil {
-                log.Println("Handshake response err: ", err)
+                log.Println("validateSubscribers response err: ", err)
                 log.Println("Removing subscriber: ", callback)
                 deadSubscribers[name] = callback
                 wg.Done()
@@ -146,6 +187,120 @@ func broadcast(store map[string]string) {
     wg.Wait()
 }
 
+func handleGet(w http.ResponseWriter, r *http.Request) {
+    var (
+        res getRes
+        req getReq
+    )
+    res.Status = "error"
+    defer func() {
+        json, err := json.Marshal(res)
+        if err != nil {
+            log.Println(err)
+        }
+        w.Write(json)
+    }()
+    decoder := json.NewDecoder(r.Body)
+    err := decoder.Decode(&req)
+    if err != nil {
+        res.Message = "Json req decoding error"
+        return
+    }
+    if req.Password != pass {
+        res.Message = "Wrong password"
+        return
+    }
+    res.Data = kv
+    res.Status = "success"
+}
+
+func handleSet(w http.ResponseWriter, r *http.Request) {
+    var (
+        res setRes
+        req setReq
+    )
+    res.Status = "error"
+    defer func() {
+        json, err := json.Marshal(res)
+        if err != nil {
+            log.Println(err)
+        }
+        w.Write(json)
+    }()
+    decoder := json.NewDecoder(r.Body)
+    err := decoder.Decode(&req)
+    if err != nil {
+        res.Message = "Json req decoding error"
+        return
+    }
+    if req.Password != pass {
+        res.Message = "Wrong password"
+        return
+    }
+    kv[req.Key] = req.Value
+    res.Status = "success"
+}
+
+func handleDelete(w http.ResponseWriter, r *http.Request) {
+    var (
+        res delRes
+        req delReq
+    )
+    res.Status = "error"
+    defer func() {
+        json, err := json.Marshal(res)
+        if err != nil {
+            log.Println(err)
+        }
+        w.Write(json)
+    }()
+    decoder := json.NewDecoder(r.Body)
+    err := decoder.Decode(&req)
+    if err != nil {
+        res.Message = "Json req decoding error"
+        return
+    }
+    if req.Password != pass {
+        res.Message = "Wrong password"
+        return
+    }
+    delete(kv, req.Key)
+    res.Status = "success"
+}
+
+func handleBroadcastNow(w http.ResponseWriter, r *http.Request) {
+    var (
+        req broadcastReq
+        res broadcastRes
+    )
+    res.Status = "error"
+    defer func() {
+        json, err := json.Marshal(res)
+        if err != nil {
+            log.Println(err)
+        }
+        w.Write(json)
+    }()
+    decoder := json.NewDecoder(r.Body)
+    err := decoder.Decode(&req)
+    if err != nil {
+        res.Message = "Json req decoding error"
+        return
+    }
+    if req.Password != pass {
+        res.Message = "Wrong password"
+        return
+    }
+    deadSubscribers := validateSubscribers(kv)
+    log.Println("validateSubscribers done. Dead subscribers: ", deadSubscribers)
+    kv = removeDeadSubscribers(kv, deadSubscribers)
+    log.Println("Dead subscribers removed")
+    broadcast(kv)
+    log.Println("Broadcast done")
+    res.Status = "success"
+    res.Message = "Broadcast done"
+}
+
 func extractSubscribers(store map[string]string) map[string]string {
     subscribers := make(map[string]string)
     for k, v := range store {
@@ -176,6 +331,10 @@ func init() {
 
 func main() {
     http.HandleFunc("/attach", handleAttach)
+    http.HandleFunc("/broadcastnow", handleBroadcastNow)
+    http.HandleFunc("/get", handleGet)
+    http.HandleFunc("/set", handleSet)
+    http.HandleFunc("/del", handleDelete)
     err := http.ListenAndServe(fmt.Sprintf(":%v", port), nil)
     if err != nil {
         log.Fatal("ListenAndServe: ", err)
